117ebe9b0d6182d391bac2277651fe8521d78f79
Ollitak <olli.takaanttila@gmail.com>
2022-01-17 12:40:02 +0200

    add simple express skeleton

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Aasdasds")
+const express = require('express');
+
+const app = express();
+const PORT = 3001 | process.env.PORT
+
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
