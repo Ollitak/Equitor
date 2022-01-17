1822f9921c708b1810e94752bac7b290010a518c
Ollitak <olli.takaanttila@gmail.com>
2022-01-17 14:50:11 +0200

    create simple api endpoints

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
@@ -5,9 +6,48 @@ const app = express();
 const PORT = 3001 | process.env.PORT;
 
 
+let analyses = [
+    {
+        id: "asd123",
+        name: "Kalle",
+        username: "K123",
+        passwordHash: "ad(/SA(FYSIDHUGFG/&S&FD(/SYDHFN",
+        ownAnalyses: ["ID1", "ID2", "ID3", "ID4"]
+    },
+    {
+        id: "asd1234",
+        name: "Seppo",
+        username: "Sepo",
+        passwordHash: "ad(/SA(FYSIDHUGFG/&S&FD(/SYDHFN",
+        ownAnalyses: ["ID1", "ID2", "ID3", "ID4"]
+    },
+    {
+        id: "asd12345",
+        name: "Jaakko",
+        username: "JJJ",
+        passwordHash: "ad(/SA(FYSIDHUGFG/&S&FD(/SYDHFN",
+        ownAnalyses: ["ID1", "ID2", "ID3", "ID4"]
+    }
+];
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send("test");
+    res.send(analyses);
 });
+
+app.get("/:id", (req, res) => {
+    const id = req.params.id;
+    res.json(analyses.find(analyse => analyse.id === id));
+});
+
+app.post("/", (req, res) => {
+    const analysis = req.body;
+    analyses = analyses.concat(analysis);
+    res.json(analysis);
+});
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
