bcf2acdabaa24fcac559b79482c9e61ef6955f69
Ollitak <olli.takaanttila@gmail.com>
2022-01-17 16:04:06 +0200

    add dotenv and config.js for env configuration

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const analysesRouter = require("./controllers/analyses");
 const middleware = require("./utils/middleware"); 
+const config = require("./utils/config.js");
 
 const app = express();
+
+console.log(`connecting to ${config.MONGODB_URI}`);
+
+
 
 app.use(express.json());
 app.use("/analyses", analysesRouter);
