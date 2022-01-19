21d9b10497f902172cdb8a7a3d5dd4e37fd16f43
Ollitak <olli.takaanttila@gmail.com>
2022-01-19 13:23:16 +0200

    fix analyses controller in backend

--- controllers/analyses.js
+++ controllers/analyses.js
@@ -54,7 +54,7 @@ analysisRouter.delete("/:id", async (req, res, next) => {
     const id = req.params.id;
     try {
         await Analysis.findByIdAndRemove(id);
-        res.status(204).json.end(); 
+        res.status(204).end(); 
     } catch(e) {
         next(e);
     }
