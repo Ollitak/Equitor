8ecec2167d5c9e0653a599cec7641fb6a430c376
Olli Taka-Anttila <olli.takaanttila@gmail.com>
2022-01-24 16:29:11 +0200

    single analysis view broken into components

--- client/src/components/SingleAnalysisView.js
+++ client/src/components/SingleAnalysisView.js
@@ -7,8 +7,105 @@ import {
   Header,
   Segment,
   Label,
-  Image
+  Image,
+  Divider,
+  Feed,
+  Icon,
+  Rating
 } from "semantic-ui-react";
+
+const SummaryInformation = ({ analysis }) => {
+  return (
+    <Grid.Column width={4} verticalAlign="center">
+      <Grid.Row columns={1} style={{ marginBottom:"1.5em" }}>
+        <Grid.Column>
+          <Segment vertical >
+            <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Equity</Header>
+            <Image verticalAlign="middle" size="small" src={analysis.stockInformation.logoUrl}/>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={1} style={{ marginBottom:"1.5em" }}>
+        <Grid.Column>
+          <Segment vertical>
+            <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Analysis by</Header>
+            <Header as="h3" color="blue" style={{ fontSize:"1.5em" }}>StockWizard</Header>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={1} style={{ marginBottom:"1.5em" }}>
+        <Grid.Column>
+          <Segment vertical>
+            <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Recommendation</Header>
+            <Label color="green" size="huge">BUY</Label>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row columns={1}>
+        <Grid.Column>
+          <Segment vertical>
+            <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Target price</Header>
+            <Label tag color="black" size="huge"> € {analysis.stockPriceEstimate}</Label>
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid.Column>
+  );
+
+};
+const AnalysisSection = ({ analysis }) => {
+  return(
+    <Grid.Row>
+      <SummaryInformation analysis={analysis} />
+      <Grid.Column width={12}>
+        <Header as="h1" style={{ fontSize:"2.5em" }}>{analysis.title}</Header>
+        <p style={{ fontSize:"1.2em" }}>{analysis.description}</p>
+      </Grid.Column>
+    </Grid.Row>
+  );
+};
+
+const CommentSection = ({ analysis }) => {
+  return(
+    <Feed>
+      <Grid.Row columns={1} style={{ marginTop:"2em" }}>
+        <Grid.Column>
+          <Divider
+            as='h4'
+            className='header'
+            horizontal
+            style={{ margin: "3em 0em", textTransform: "uppercase" }}
+          >
+            <a>Comments</a>
+          </Divider>
+          {analysis.comments.map((comment, id) => {
+            return (
+              <Feed.Event key={id} style={{ marginBottom: "1em" }}>
+                <Feed.Label>
+                  <Icon name="comment" color="grey"></Icon>
+                </Feed.Label>
+                <Feed.Content>
+                  <Feed.Date>4 days ago</Feed.Date>
+                  <Feed.Summary>
+                        Commented and rated by <a>{comment.user.username}</a>
+                  </Feed.Summary>
+                  <Rating disabled size="tiny" icon="star" defaultRating="2" maxRating="4" />
+                  <Feed.Extra>
+                    {comment.content}
+                  </Feed.Extra>
+                  <Feed.Meta>
+                  </Feed.Meta>
+                </Feed.Content>
+              </Feed.Event>
+            );
+          })}
+        </Grid.Column>
+      </Grid.Row>
+    </Feed>
+  );
+};
+
+
 
 const SingleAnalysisView = () => {
   const { id } = useParams();
@@ -27,59 +124,11 @@ const SingleAnalysisView = () => {
   return (
     <Segment style={{ padding:"1em" }} vertical>
       <Grid stackable container>
-
-
-        <Grid.Column width={4} verticalAlign="center">
-          <Grid.Row columns={1} style={{ marginBottom:"1.5em" }}>
-            <Grid.Column>
-              <Segment vertical >
-                <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Equity name</Header>
-                <Image verticalAlign="middle" size="small" src={analysis.stockInformation.logoUrl}/>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row columns={1} style={{ marginBottom:"1.5em" }}>
-            <Grid.Column>
-              <Segment vertical>
-                <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Analysis by</Header>
-                <Header as="h3" color="blue" style={{ fontSize:"1.5em" }}>StockWizard</Header>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row columns={1} style={{ marginBottom:"1.5em" }}>
-            <Grid.Column>
-              <Segment vertical>
-                <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Recommendation</Header>
-                <Label color="green" size="huge">BUY</Label>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row columns={1}>
-            <Grid.Column>
-              <Segment vertical>
-                <Header dividing as="h3" style={{ fontSize:"1.5em" }}>Target price</Header>
-                <Label tag color="black" size="huge"> € {analysis.stockPriceEstimate}</Label>
-              </Segment>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid.Column>
-
-
-        <Grid.Column width={12}>
-          <Header as="h1" style={{ fontSize:"2.5em" }}>{analysis.title}</Header>
-          <p style={{ fontSize:"1.2em" }}>{analysis.description}</p>
-        </Grid.Column>
-
-
-
-
-
+        <AnalysisSection analysis={analysis} />
+        <CommentSection analysis={analysis} />
       </Grid>
     </Segment>
   );
-
 };
 
 export default SingleAnalysisView;
