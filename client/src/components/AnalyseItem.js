0550f87f0a51393e181ab43f74282f72c3c463fc
Ollitak <olli.takaanttila@gmail.com>
2022-01-25 18:34:32 +0200

    analysis item styling with semantic ui

--- client/src/components/AnalyseItem.js
+++ client/src/components/AnalyseItem.js
@@ -1,53 +1,94 @@
 import React from "react";
 import "./AnalyseItem.css";
 import { useHistory } from "react-router-dom";
-import { deleteAnalysis } from "../reducers/analysisReducer";
-import { useDispatch } from "react-redux";
-import { Segment, Grid, Image, Label, Icon } from "semantic-ui-react";
+//import { deleteAnalysis } from "../reducers/analysisReducer";
+//import { useDispatch } from "react-redux";
+import { Feed, Segment, Button, Rating, Grid } from "semantic-ui-react";
 
-
-const AnalyseItem = ({ analysis, myPage }) => {
+//myPage
+const AnalyseItem = ({ analysis }) => {
   const history = useHistory();
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
 
+  const postedAgo = getAgo(analysis.date);
+
+  /*
   const removeItem = async (id) => {
     dispatch(deleteAnalysis(id));
   };
-
+*/
   const moveToSingleView = () => {
     history.push(`analysis/${analysis.id}`);
   };
 
   return(
-    <Segment color="olive" onClick={moveToSingleView} style={{ margin:"auto", height:"6em", maxWidth: "40em", marginBottom:"1em" }}>
-      <Grid>
-        <Grid.Column width={3}>
-          <Image src={analysis.stockInformation.logoUrl} fluid style={{ height:"4em" }}/>
-        </Grid.Column>
-        <Grid.Column width={13}>
-          <Grid.Row>
-            a new analysis on <b>{analysis.stockInformation.name}</b> was posted 1 hour ago
-          </Grid.Row>
-          <Grid.Row style={{ marginTop:"1em" }}>
-            <Label color="black">
-              <Icon name="like"></Icon>
-              25
-            </Label>
-            <Label color="black">
-              <Icon name="comments"></Icon>
-              3
-            </Label>
-          </Grid.Row>
-          {/* Only allow delete if user is on myPage */}
-          {
-            myPage
-              ? <button onClick={() => removeItem(analysis.id)}> delete </button>
-              : null
-          }
-        </Grid.Column>
-      </Grid>
+    <Segment style={{ margin: "auto", marginBottom:"1em", background:"white", maxWidth:600 }}>
+      <Feed>
+        <Feed.Event>
+          <Feed.Content>
+            <Feed.Summary>
+              <Feed.User>{analysis.user.username}</Feed.User>
+              {` posted a new analysis on ${analysis.stockInformation.name}`}
+            </Feed.Summary>
+            <Feed.Meta>
+              { postedAgo === 0
+                ?<Feed.Date>under hour ago </Feed.Date>
+                :<Feed.Date>{postedAgo} hours ago </Feed.Date>
+              }
+              <Rating style={{ marginLeft: "0em", marginTop:"0.5em" }} disabled defaultRating={2} maxRating={5} />
+              <Grid style={{ marginLeft: "0em" }}>
+                <Grid.Row>
+                  {analysis.keyWords.map((keyWord, id) => {
+                    return (
+                      <div key={id} style={{ backgroundColor:"black", borderRadius:"2em",  margin:"0.5em", padding:"0.5em" }}>
+                        <p style={{ fontWeight:"bold", color:"white", fontSize:"0.9em" }}>{keyWord}</p>
+                      </div>
+                    );
+                  })}
+                </Grid.Row>
+              </Grid>
+            </Feed.Meta>
+          </Feed.Content>
+          <Grid verticalAlign="middle">
+            <Grid.Column >
+              <Button onClick={moveToSingleView} compact style={{ height:"4em", color:"white", backgroundColor:"rgb(10, 40, 230)" }}>
+                Check it out!
+              </Button>
+            </Grid.Column>
+          </Grid>
+        </Feed.Event>
+      </Feed>
     </Segment>
   );
 };
+
+
+/* This ugly function parses the date when analysis was posted and
+ returs time difference between that date and today in hours  */
+const getAgo = (unparsedDate) => {
+  const dateAndTime = unparsedDate.split(":");
+  const postDate = dateAndTime[0];
+  const postTime = dateAndTime[1];
+  const splittedDate = postDate.split("-");
+  const dateAndHours = splittedDate[2].split("T");
+  const splittedTime = postTime.split(":");
+
+  const year = splittedDate[0];
+  /* JavaScript counts months from 0 to 11 */
+  const month = splittedDate[1]-1;
+  const day = dateAndHours[0];
+  /*Database has times in GMT+0 timezone, whereas Finland is in GMT+2.
+  For now, just add +2 hours to dates retreived from database */
+  const hour = parseInt(dateAndHours[1])+2;
+  const minute = splittedTime[0];
+
+  const postedOn = new Date(year, month, day, hour, minute);
+  const today = new Date();
+  /* The subtraction returns the difference between the two dates in milliseconds.
+   36e5 is notation for 60*60*1000 */
+  return Math.trunc(Math.abs(today - postedOn)/36e5);
+};
+
+
 
 export default AnalyseItem;
