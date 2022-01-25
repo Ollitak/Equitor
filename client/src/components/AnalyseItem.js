aeba327d4399c848b36b26b42fcbbdf98a1f1541
Ollitak <olli.takaanttila@gmail.com>
2022-01-25 19:07:24 +0200

    add getAgo and getAverageRating functions

--- client/src/components/AnalyseItem.js
+++ client/src/components/AnalyseItem.js
@@ -10,7 +10,12 @@ const AnalyseItem = ({ analysis }) => {
   const history = useHistory();
   //const dispatch = useDispatch();
 
+
+  /* Determine how many hours ago the analysis was posted */
   const postedAgo = getAgo(analysis.date);
+
+  /* Determine the average of given ratings */
+  const averageRating = getAverageRating(analysis.comments);
 
   /*
   const removeItem = async (id) => {
@@ -35,7 +40,7 @@ const AnalyseItem = ({ analysis }) => {
                 ?<Feed.Date>under hour ago </Feed.Date>
                 :<Feed.Date>{postedAgo} hours ago </Feed.Date>
               }
-              <Rating style={{ marginLeft: "0em", marginTop:"0.5em" }} disabled defaultRating={2} maxRating={5} />
+              <Rating style={{ marginLeft: "0em", marginTop:"0.5em" }} disabled defaultRating={averageRating} maxRating={5} />
               <Grid style={{ marginLeft: "0em" }}>
                 <Grid.Row>
                   {analysis.keyWords.map((keyWord, id) => {
@@ -63,6 +68,12 @@ const AnalyseItem = ({ analysis }) => {
 };
 
 
+const getAverageRating = (comments) => {
+  if(!comments || comments.length===0) return 0;
+  const average = comments.reduce((acc, comment) => acc + comment.rating, 0) / comments.length;
+  return average;
+};
+
 /* This ugly function parses the date when analysis was posted and
  returs time difference between that date and today in hours  */
 const getAgo = (unparsedDate) => {
@@ -78,7 +89,8 @@ const getAgo = (unparsedDate) => {
   const month = splittedDate[1]-1;
   const day = dateAndHours[0];
   /*Database has times in GMT+0 timezone, whereas Finland is in GMT+2.
-  For now, just add +2 hours to dates retreived from database */
+  For now, just add +2 hours to dates retreived from database. For sure,
+  not a good practice. */
   const hour = parseInt(dateAndHours[1])+2;
   const minute = splittedTime[0];
 
