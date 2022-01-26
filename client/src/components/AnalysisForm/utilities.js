9511e5a55c2b85c3950f46500fa63f1bf8254568
Ollitak <olli.takaanttila@gmail.com>
2022-01-26 19:57:59 +0200

    update company list

--- client/src/components/AnalysisForm/utilities.js
+++ client/src/components/AnalysisForm/utilities.js
@@ -1,25 +1,46 @@
 export const stockSelection = [
   {
-    label: "Valmet (VALMT)",
-    value: "Valmet (VALMT)",
+    label: "Valmet",
+    value: "Valmet",
     name: "Valmet Oyj",
     ticker: "VALMT",
     logoUrl: "https://www.valmet.com/globalassets/media/media-gallery/logos/valmet_logo_rgb.jpg"
   },
   {
-    label: "Wärtsilä (WRT)",
-    value: "Wärtsilä (WRT)",
+    label: "Wärtsilä",
+    value: "Wärtsilä",
     name: "Wärtsilä Oyj",
     ticker: "WRT",
     logoUrl:"https://www.wartsila.com/images/default-source/brand-portal/logo/w-logo-1.png?sfvrsn=9b828545_6"
   },
   {
-    label: "Nokia (NOK)",
-    value: "Nokia (NOK)",
+    label: "Nokia",
+    value: "Nokia",
     name: "Nokia Oyj",
     ticker: "NOK",
     logoUrl: "https://www.nokia.com/sites/default/files/styles/scale_720_no_crop/public/media/nokia_white_logo.png"
-  }
+  },
+  {
+    label: "Nordea",
+    value: "Nordea",
+    name: "Nordea Bank Oyj",
+    ticker: "NDA",
+    logoUrl: "https://www.nordea.fi/Images/147-169221/Nordea-logo%20(2017).svg"
+  },
+  {
+    label: "Raisio",
+    value: "Raisio",
+    name: "Raisio Oyj",
+    ticker: "RAIVV",
+    logoUrl: "https://www.raisio.com/wp-content/themes/everblox/dist/78716bfdd2a03985bbfe.svg"
+  },
+  {
+    label: "Kone",
+    value: "Kone",
+    name: "Kone Oyj",
+    ticker: "KNEBV",
+    logoUrl: "https://www.kone.com/fi/Images/Kone-header-lg_tcm18-99221.png?v=1"
+  },
 ];
 
 export const keyWordOptions = [
