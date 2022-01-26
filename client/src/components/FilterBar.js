70458bc73c6f9665bbe26961ab8372a3cf92a0ad
Ollitak <olli.takaanttila@gmail.com>
2022-01-26 10:44:35 +0200

    implement filter reset

--- client/src/components/FilterBar.js
+++ client/src/components/FilterBar.js
@@ -1,24 +1,38 @@
 import React from "react";
-import { Segment, Form  } from "semantic-ui-react";
+import { Segment, Form, Button  } from "semantic-ui-react";
 import Select from "react-select";
 import { stockSelection } from "./AnalysisForm/utilities";
-import { useDispatch } from "react-redux";
-import { setCompanyFilter } from "../reducers/filterReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { setCompanyFilter, resetFilters } from "../reducers/filterReducer";
 
 
 const FilterBar = () => {
   const dispatch = useDispatch();
-
+  const companyFilter = useSelector(state => state.filter.companyFilter);
+  console.log(companyFilter);
   return (
     <Segment inverted style={{ borderRadius: 0 }}>
       <Segment inverted style={{ margin: "auto", padding:"0em", maxWidth:600 }}>
 
         <Form.Field>
           <label>Filter by company</label>
+          {/* Select's value attribute expects a shape of object */}
           <Select
-            styles={{ color:"red" }}
+            value={{ label: companyFilter }}
             options={stockSelection}
             onChange={(vals) => dispatch(setCompanyFilter(vals.name))}
+          />
+          <Button
+            compact
+            style={{
+              marginTop:"1em",
+              height:"3em",
+              color:"white",
+              backgroundColor:"black",
+              border: "solid 1px white"
+            }}
+            content="Reset"
+            onClick={() => dispatch(resetFilters())}
           />
         </Form.Field>
 
--- client/src/reducers/filterReducer.js
+++ client/src/reducers/filterReducer.js
@@ -6,6 +6,8 @@ const reducer  = (state = initialState, action) => {
   switch(action.type) {
   case "SET_COMPANY_FILTER":
     return { ...state, companyFilter: action.filter };
+  case "RESET_FILTERS":
+    return initialState;
   default:
     return state;
   }
@@ -16,6 +18,12 @@ export const setCompanyFilter = (filter) => {
   return {
     type: "SET_COMPANY_FILTER",
     filter
+  };
+};
+
+export const resetFilters = () => {
+  return {
+    type: "RESET_FILTERS"
   };
 };
 
