60b829e265696dc7985b75b79ed12ea31d174cf3
Ollitak <olli.takaanttila@gmail.com>
2022-01-27 13:47:04 +0200

    fix Feed component

--- client/src/components/Feed/Feed.js
+++ client/src/components/Feed/Feed.js
@@ -3,7 +3,7 @@ import { Button } from "semantic-ui-react";
 import FeedItem from "../FeedItem";
 import FilterBar from "./FilterBar";
 import { useSelector } from "react-redux";
-import prepareAnalyses, { orderByDateASC, orderByDateDESC, orderByRatingASC, orderByRatingDESC } from "../../utilities/prepareAnalyses";
+import prepareAnalyses from "../../utilities/prepareAnalyses";
 
 /* If showFilterBar is set to true, renders FilterBar. Else, renders
 button used to set showFilterBar true. */
@@ -33,34 +33,10 @@ redux store and applies the filters to the analyses. Then, it renders filter com
 and analysis feed. */
 const Feed = () => {
   var analyses = useSelector(state => state.analyses);
+
   /* prepareAnalyses adds fields for every analysis element indicating how many hours ago
   the analysis was posted and what is its average rating */
   analyses = prepareAnalyses(analyses);
-
-  const filter = useSelector(state => state.filter);
-
-  if(filter.companyFilter) {
-    analyses = analyses.filter(analysis =>
-      analysis.stockInformation.name === filter.companyFilter
-    );
-  }
-
-  if(filter.keywordFilter) {
-    analyses = analyses.filter(analysis =>
-      analysis.keyWords.includes(filter.keywordFilter)
-    );
-  }
-
-  switch(filter.orderingFilter) {
-  case("Most recent"):
-    analyses = orderByDateASC(analyses); break;
-  case("Oldest"):
-    analyses = orderByDateDESC(analyses); break;
-  case("Lowest rated"):
-    analyses = orderByRatingASC(analyses); break;
-  case("Highest rated"):
-    analyses = orderByRatingDESC(analyses);
-  }
 
   return(
     <div>
