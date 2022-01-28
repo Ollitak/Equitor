685a9d6cdcb6fad2296612b8d722f8c08afefc1e
Ollitak <olli.takaanttila@gmail.com>
2022-01-28 13:42:56 +0200

    delete unnecessary eslint rules

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -20,11 +20,6 @@ module.exports = {
   },
   plugins: ["react", "jest"],
   rules: {
-    semi: ["error", "always"],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": ["error", "always"],
-    "arrow-spacing": ["error", { before: true, after: true }],
     "no-console": 0,
     "react/prop-types": 0
   },
